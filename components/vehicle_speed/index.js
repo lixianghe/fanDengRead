529894592a4e070f70e765e3eeea0d37c389f131
zhuleishan <473668281@qq.com>
2021-05-28 15:20:44 +0800

    修改车机行驶遮挡组件

--- components/vehicle_speed/index.js
+++ components/vehicle_speed/index.js
@@ -1,3 +1,4 @@
+const app = getApp();
 Component({
   /**
    * 组件的属性列表
@@ -5,34 +6,22 @@ Component({
   properties: {
     isWhole: {
       type: Boolean,
-      value: true,
+      value: false,
     },
-    vehicleTips: {
-      type: String,
-      value: "请遵守交通法规，请勿在驾驶时扫描二维码",
-    },
-    confirmText: {
-      type: String,
-      value: "我知道了",
-    },
-    customStyle:{
-      type: String,
-      value: "",
-    },
-    btnStyle:{
-      type: String,
-      value: "",
-    }
   },
   /**
    * 组件的初始数据
    */
   data: {
+    vehicleTips: "请遵守交通法规，请勿在驾驶时扫描二维码",
+    confirmText: "我知道了",
     showQrcodeMask: false,
-    vehicleTipsArray:[]
+    vehicleTipsArray: [],
+    callbackkey: '',
   },
   // 页面初始化
   attached: function () {
+    this.callbackkey = Date.now();
     this.initialization();
   },
   // 页面卸载
@@ -50,38 +39,79 @@ Component({
   methods: {
     // 页面初始化
     initialization() {
-      if(!this.data.isWhole) this.setData({
-        vehicleTipsArray:this.data.vehicleTips.split('，')
-      })
-      if (wx.canIUse("getVehicleSpeedSync")) {
-        let { state } = wx.getVehicleSpeedSync();
+      if (!this.data.isWhole)
         this.setData({
-          showQrcodeMask: state != "stopped",
+          vehicleTipsArray: this.data.vehicleTips.split("，"),
+        });
+      if (wx.canIUse("getRestrictions")) {
+        wx.getRestrictions({
+          type: 1,
+          success: res => {
+            this.setData({
+              showQrcodeMask: res.restricted,
+            });
+          },
+          fail: err => {
+            this.setData(
+              {
+                showQrcodeMask: false,
+              },
+              () => {
+                wx.showToast({
+                  title: err.errMsg,
+                  icon: "none",
+                  duration: 1500,
+                  mask: false,
+                });
+              }
+            );
+          },
         });
       }
       this.watcVehicleSpeed();
     },
+    qrcodeCallback(that, res) {
+      that.setData({
+        showQrcodeMask: res.restricted,
+      });
+    },
     // 获取车机行驶状态
     watcVehicleSpeed() {
-      if (wx.canIUse("onVehicleSpeedChange")) {
-        wx.onVehicleSpeedChange((res) => {
-          this.setData({
-            showQrcodeMask: res.state != "stopped",
+      if (!app.qrcode_listeners) app.qrcode_listeners = [];
+      app.qrcode_listeners.push({key:this.callbackkey,func:this.qrcodeCallback,page:this});
+      if (!app.qrcode_callback_registerd) {
+        app.qrcode_callback_registerd = true;
+        app.qrcode_callback = (res) => {
+          app.qrcode_listeners.forEach((item) => {
+            item.func(item.page,res);
           });
-        });
+        };
+        if (wx.canIUse("onRestrictionsChange"))
+          wx.onRestrictionsChange({ type: 1, callback: app.qrcode_callback });
       }
     },
     // 销毁车机行驶状态监听
     destructionWatc() {
-      if (wx.canIUse("offVehicleSpeedChange")) {
-        wx.offVehicleSpeedChange((res) => {
-          this.setData({
-            showQrcodeMask: res.state != "stopped"
-          });
-        });
+      if(!app.qrcode_listeners) return;
+      let _index = -1;
+      app.qrcode_listeners.forEach((item, index) => {
+        if(item.key == this.callbackkey){
+          _index = index;
+        }
+      })
+      if(_index !== -1){
+        app.qrcode_listeners.splice(_index, 1);
+      }
+      if(!app.qrcode_listeners.length){
+        if(wx.canIUse("offRestrictionsChange")){
+          wx.offRestrictionsChange({ type: 1 });
+        }
+        delete app.qrcode_listeners;
+        delete app.qrcode_callback;
+        delete app.qrcode_callback_registerd;
       }
       this.setData({
-        showQrcodeMask: false
+        showQrcodeMask: false,
       });
     },
   },
